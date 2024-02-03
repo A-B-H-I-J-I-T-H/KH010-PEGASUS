6bb37510b4e14458079df1ecd251291d74b874ad
Athulkrishna-S <119509884+Athulkrishna-S@users.noreply.github.com>
2024-02-03 20:44:07 +0530

    Updated installmentModel.js

--- models/installmentModel.js
+++ models/installmentModel.js
@@ -1,5 +1,6 @@
 // models/installmentModel.js
 const { MongoClient } = require('mongodb');
+const sendEmail = require('../utils/emailSender');
 
 const mongoUri = process.env.MONGO_URI;
 const dbName = process.env.DB_NAME;
@@ -12,9 +13,27 @@ class InstallmentModel {
       await client.connect();
       const db = client.db(dbName);
 
-      // Search for documents in installment_date collection with inst_date equal to the provided day
       const collection = db.collection('installment_date');
       const result = await collection.find({ inst_date: day }).toArray();
+
+      if (result.length > 0) {
+        for (const installment of result) {
+          const username = installment.username;
+          const topic = installment.topic;
+
+          const userCollection = db.collection(username);
+          const userDocument = await userCollection.findOne({ topic: topic });
+
+          if (userDocument && userDocument.email) {
+            const toEmail = userDocument.email;
+            const emailSubject = 'Payment Due';
+            const emailText = `Dear ${username},\n\nYour payment for ${topic} is due. Please make the payment promptly.`;
+
+            // Call the sendEmail function to send the email
+            await sendEmail(toEmail, emailSubject, emailText);
+          }
+        }
+      }
 
       return result;
     } finally {
