94a5b695b49aee89f6b226941a585c0b2414e41d
Athulkrishna-S <119509884+Athulkrishna-S@users.noreply.github.com>
2024-02-04 12:41:40 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,35 @@
-// models/userModel.js
 const { MongoClient } = require('mongodb');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 // Load environment variables using dotenv or any other method
 require('dotenv').config();
 
 // Use process.env to get environment variables
 const mongoUri = process.env.MONGO_URI;
-const dbName = process.env.DB_NAME ;
+const dbName = process.env.DB_NAME;
 
 class UserModel {
   async getUserByUsernameAndPassword(username, password) {
+    // Sanitize inputs
+    const sanitizedUsername = validator.escape(username);
+    const sanitizedPassword = validator.escape(password);
+
     const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
       await client.connect();
       const db = client.db(dbName);
 
-      const user = await db.collection('users').findOne({ username, password });
+      // Retrieve the user with the sanitized username
+      const user = await db.collection('users').findOne({ username: sanitizedUsername });
 
-      return user;
+      // Check if the user exists and if the hashed password matches
+      if (user && await bcrypt.compare(sanitizedPassword, user.password)) {
+        return user; // Passwords match
+      } else {
+        return null; // User not found or passwords do not match
+      }
     } finally {
       await client.close();
     }
