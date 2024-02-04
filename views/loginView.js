1004c8f14869cb9030347b0941bcbeaf1181e13b
Athulkrishna-S <119509884+Athulkrishna-S@users.noreply.github.com>
2024-02-04 10:02:00 +0530

    Update loginView.js

--- views/loginView.js
+++ views/loginView.js
@@ -3,18 +3,11 @@ const path = require('path');
 
 class LoginView {
   renderLogin(res, flashMessage) {
-    // Render the login.ejs file with flashMessage
     res.render('login', { flashMessage });
-    console.log('hello')
   }
 
   renderSuccess(user) {
     console.log(`Login successful. Welcome, ${user.username}!`);
-  }
-
-  renderFailure(res, flashMessage) {
-    // Redirect to the login page with flash message
-    this.renderLogin(res, flashMessage);
   }
 
   renderError(res) {
