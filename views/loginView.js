5bf8ea66d162e66b18c6d544adc04ea7d2078033
Athulkrishna-S <119509884+Athulkrishna-S@users.noreply.github.com>
2024-02-04 14:42:24 +0530

    Update loginView.js

--- views/loginView.js
+++ views/loginView.js
@@ -6,8 +6,9 @@ class LoginView {
     res.render('login', { flashMessage });
   }
 
-  renderSuccess(user) {
+  renderSuccess(res,user) {
     console.log(`Login successful. Welcome, ${user.username}!`);
+    res.redirect(`/${user.username}/dashboard`);
   }
 
   renderError(res) {
